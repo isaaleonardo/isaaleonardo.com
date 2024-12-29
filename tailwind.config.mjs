fed609048d322c0b92d59ae85a1eb79c4f883343
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2024-12-28 21:24:39 -0400

    Set breakpoints

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,10 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		screens: {
+			'sm': '425px',
+			'md': '768px',
+		}
 	},
 	plugins: [],
 }
