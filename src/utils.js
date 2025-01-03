aee56d53a827025852794400b620baa4c4bcb216
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2025-01-03 08:19:29 -0400

    Order posts by date in descending order

--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,12 @@ export function formatDate(date) {
     day: 'numeric'
   })
 }
+
+export function sortByDateDesc(array) {
+  return array.sort((a, b) => {
+    const dateA = new Date(a.data.date);
+    const dateB = new Date(b.data.date);
+
+    return dateB - dateA;
+  });
+}
